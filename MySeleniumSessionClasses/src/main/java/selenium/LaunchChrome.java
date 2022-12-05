cae72775d1ebee75dd755c10eec0727c542bf046
debuggerboy <debuggerboy@gmail.com>
2022-12-05 14:05:48 +0530

    try adding chromeoptions

--- MySeleniumSessionClasses/src/main/java/selenium/LaunchChrome.java
+++ MySeleniumSessionClasses/src/main/java/selenium/LaunchChrome.java
@@ -4,9 +4,13 @@ import or.openqa.selenium.WebDriver;
 import or.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
-    public static void main(string[] args) {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public static void main(string[] args) throws IOException, InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--window-size=1200x600");
+        chromeOptions.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get ("https://www.google.com");
         System.out.println(driver.getTitle());
     }
